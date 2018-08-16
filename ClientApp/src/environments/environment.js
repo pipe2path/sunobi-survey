6be9f4293213cfcac78e5cbf581a516e43ad0488
unknown <kevinmenezes@yahoo.com>
2018-08-16 14:34:58 -0700

    changed offer color

---
+++ ClientApp/src/environments/environment.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.environment = {
+    production: false,
+    apiUrl: 'http://localhost:56362/api'
+};
+//# sourceMappingURL=environment.js.map
