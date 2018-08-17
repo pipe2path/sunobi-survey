81207875bfef7ea462c551de10c5259b8274d023
Kevin Menezes <kevinmenezes@yahoo.com>
2018-08-16 19:37:06 -0700

    updated form and validation

--- ClientApp/src/environments/environment.js
+++ ClientApp/src/environments/environment.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = {
     production: false,
-    apiUrl: 'http://localhost:56362/api'
+    apiUrl: 'http://localhost:1352/api'
 };
 //# sourceMappingURL=environment.js.map
--- ClientApp/src/environments/environment.ts
+++ ClientApp/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:56362/api'
+  apiUrl: 'http://localhost:1352/api'
 };
